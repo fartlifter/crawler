d705cbc7cb98c3715d87350727eee491aa35f6f6
fartlifter <hdssherpa@gmail.com>
2025-06-06 20:32:01 +0900

    Update test.py

--- test.py
+++ test.py
@@ -170,5 +170,5 @@ if st.button("📥 기사 수집 시작"):
         text_block = ""
         for art in articles:
             text_block += f"△{art['title']}\n-" + art["content"].replace("\n", " ").strip() + "\n\n"
-        st.code(text_block.strip(), width=100, language="markdown")
+        st.code(text_block.strip(), language="markdown")
         st.caption("위 내용을 복사해서 사용하세요.")
